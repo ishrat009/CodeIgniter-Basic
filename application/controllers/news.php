ceb2e90f872d258c473357b0c202a7f6fba757ff
Ishrat Sharmin <ishrat.cste@gmail.com>
2014-10-16 17:45:16 +0600

    Corrected controller

--- application/controllers/news.php
+++ application/controllers/news.php
@@ -9,19 +9,18 @@ class News extends CI_Controller
 {
   public function __construct() 
   {
-      parent::__construct();
-  
-      $this->load->model('login_model');
+      parent::__construct();  
+      $this->load->model('news_model');
   }
   
-  public function showNews()
+  public function showNews($id=2)
   {
-      $news = $this->login_model->getNews($id);
+      $news = $this->news_model->getNews($id);
       
       $data['title'] = $news['title'];
       $data['body'] = $news['body'];
       
-      $this->load->view('news_article',$data);
+      $this->load->view('news_article', $data);
   }
   
 }
