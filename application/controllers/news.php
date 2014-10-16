21a409a38245cfd8a197a38fce6940d37e488121
Ishrat Sharmin <ishrat.cste@gmail.com>
2014-10-16 16:39:51 +0600

    function showNews
    to load the views according to model query

--- application/controllers/news.php
+++ application/controllers/news.php
@@ -14,4 +14,14 @@ class News extends CI_Controller
       $this->load->model('login_model');
   }
   
+  public function showNews()
+  {
+      $news = $this->login_model->getNews($id);
+      
+      $data['title'] = $news['title'];
+      $data['body'] = $news['body'];
+      
+      $this->load->view('news_article',$data);
+  }
+  
 }
