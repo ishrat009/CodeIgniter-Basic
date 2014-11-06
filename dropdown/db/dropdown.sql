5c8c522dee9606a1db66e068b314541053d09a6a
Ishrat Sharmin <ishrat.cste@gmail.com>
2014-11-06 15:37:33 +0600

    Database has been added

---
+++ dropdown/db/dropdown.sql
@@ -0,0 +1,86 @@
+-- phpMyAdmin SQL Dump
+-- version 3.2.0.1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Nov 06, 2014 at 09:34 AM
+-- Server version: 5.1.53
+-- PHP Version: 5.3.4
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `dropdown`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dropdown_value`
+--
+
+CREATE TABLE IF NOT EXISTS `dropdown_value` (
+  `dropdown_single` varchar(255) NOT NULL,
+  `dropdown_multi` varchar(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `dropdown_value`
+--
+
+INSERT INTO `dropdown_value` (`dropdown_single`, `dropdown_multi`) VALUES
+('Electronics and Comm. Engineering', 'a:1:{i:0;s:1:"C";}'),
+('Computer Science Engineering', 'a:2:{i:0;s:1:"C";i:1;s:3:"C++";}'),
+('Computer Science Engineering', 'a:2:{i:0;s:1:"C";i:1;s:3:"C++";}'),
+('0', 'b:0;'),
+('0', 'b:0;'),
+('0', 'b:0;'),
+('0', 'b:0;'),
+('Computer Science Engineering', 'a:2:{i:0;s:1:"C";i:1;s:6:"ORACLE";}'),
+('Computer Science Engineering', 'a:2:{i:0;s:4:"JAVA";i:1;s:8:"DATABASE";}'),
+('Computer Science Engineering', 'a:2:{i:0;s:4:"JAVA";i:1;s:8:"DATABASE";}'),
+('Computer Science Engineering', 'a:2:{i:0;s:4:"JAVA";i:1;s:8:"DATABASE";}'),
+('Computer Science Engineering', 'a:2:{i:0;s:8:"DATABASE";i:1;s:3:"PHP";}'),
+('Computer Science Engineering', 'a:2:{i:0;s:8:"DATABASE";i:1;s:3:"PHP";}'),
+('0', 'b:0;'),
+('Computer Science Engineering', 'a:2:{i:0;s:8:"DATABASE";i:1;s:3:"PHP";}'),
+('Computer Science Engineering', 'a:2:{i:0;s:8:"DATABASE";i:1;s:3:"PHP";}'),
+('Computer Science Engineering', 'a:2:{i:0;s:8:"DATABASE";i:1;s:3:"PHP";}'),
+('Computer Science Engineering', 'a:2:{i:0;s:8:"DATABASE";i:1;s:3:"PHP";}'),
+('Computer Science Engineering', 'a:2:{i:0;s:8:"DATABASE";i:1;s:3:"PHP";}'),
+('0', 'b:0;'),
+('0', 'b:0;'),
+('0', 'b:0;'),
+('Electronics and Comm. Engineering', 'a:2:{i:0;s:1:"C";i:1;s:6:"ORACLE";}'),
+('Computer Science Engineering', 'a:2:{i:0;s:8:"DATABASE";i:1;s:3:"PHP";}'),
+('Computer Science Engineering', 'a:2:{i:0;s:8:"DATABASE";i:1;s:3:"PHP";}'),
+('Computer Science Engineering', 'a:2:{i:0;s:8:"DATABASE";i:1;s:3:"PHP";}'),
+('Computer Science Engineering', 'a:2:{i:0;s:8:"DATABASE";i:1;s:3:"PHP";}'),
+('Computer Science Engineering', 'a:2:{i:0;s:8:"DATABASE";i:1;s:3:"PHP";}'),
+('Computer Science Engineering', 'a:2:{i:0;s:8:"DATABASE";i:1;s:3:"PHP";}'),
+('Computer Science Engineering', 'a:2:{i:0;s:8:"DATABASE";i:1;s:3:"PHP";}'),
+('Computer Science Engineering', 'a:2:{i:0;s:8:"DATABASE";i:1;s:3:"PHP";}'),
+('Computer Science Engineering', 'a:2:{i:0;s:8:"DATABASE";i:1;s:3:"PHP";}'),
+('Computer Science Engineering', 'a:2:{i:0;s:8:"DATABASE";i:1;s:3:"PHP";}'),
+('Computer Science Engineering', 'a:2:{i:0;s:8:"DATABASE";i:1;s:3:"PHP";}'),
+('Computer Science Engineering', 'a:2:{i:0;s:8:"DATABASE";i:1;s:3:"PHP";}'),
+('Computer Science Engineering', 'a:2:{i:0;s:8:"DATABASE";i:1;s:3:"PHP";}'),
+('Computer Science Engineering', 'a:2:{i:0;s:8:"DATABASE";i:1;s:3:"PHP";}'),
+('Computer Science Engineering', 'a:2:{i:0;s:8:"DATABASE";i:1;s:3:"PHP";}'),
+('Computer Science Engineering', 'a:2:{i:0;s:8:"DATABASE";i:1;s:3:"PHP";}'),
+('Computer Science Engineering', 'a:2:{i:0;s:8:"DATABASE";i:1;s:3:"PHP";}'),
+('Computer Science Engineering', 'a:2:{i:0;s:8:"DATABASE";i:1;s:3:"PHP";}'),
+('Computer Science Engineering', 'a:2:{i:0;s:8:"DATABASE";i:1;s:3:"PHP";}'),
+('Computer Science Engineering', 'a:2:{i:0;s:8:"DATABASE";i:1;s:3:"PHP";}'),
+('Computer Science Engineering', 'a:2:{i:0;s:8:"DATABASE";i:1;s:3:"PHP";}'),
+('Mechanical Engineering', 'a:3:{i:0;s:3:"C++";i:1;s:8:"DATABASE";i:2;s:3:"PHP";}'),
+('Computer Science Engineering', 'a:2:{i:0;s:1:"C";i:1;s:6:"ORACLE";}'),
+('Computer Science Engineering', 'a:4:{i:0;s:1:"C";i:1;s:3:"C++";i:2;s:8:"DATABASE";i:3;s:3:"PHP";}'),
+('Computer Science Engineering', 'a:2:{i:0;s:8:"DATABASE";i:1;s:3:"PHP";}'),
+('Computer Science Engineering', 'a:4:{i:0;s:1:"C";i:1;s:3:"C++";i:2;s:8:"DATABASE";i:3;s:3:"PHP";}');
